ed2b9ba08d2ded103ddfd170ae533e648511ed03
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-20 21:13:03 +0800

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,15 @@
 // src/components/Login.js
 import React from 'react';
 import { supabase } from '../supabaseClient';
+import './Login.css'; // Import the new CSS file for styling
+import logo from '../../discord-icon.png';
 
 const Login = () => {
   const handleDiscordLogin = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'discord',
       options: {
-        redirectTo: 'https://cryptoknow.space'
+        redirectTo: 'https://cryptoknow.space' // Update this URL as needed
       }
     });
     if (error) {
@@ -17,6 +19,7 @@ const Login = () => {
 
   return (
     <button className="discord-login-button" onClick={handleDiscordLogin}>
+      <img src={logo} alt="Discord Logo" className="logo" />
       Log in with Discord
     </button>
   );
