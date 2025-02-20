7eeead793983e901d100334866b04d3bc32f5b60
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-20 22:13:16 +0800

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Sidebar.css';
-import logo from '../../assets/02_Cat General (Transparent).png'; // Adjust path if needed
+import logo from '../../assets/Catana Adventure.png'; // Adjust path if needed
 
 function Sidebar({ isLoggedIn }) {
   return (
