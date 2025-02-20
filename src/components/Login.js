4866357a22408b42d92d4e7cfb7c61f41ce1a66e
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-20 21:26:18 +0800

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,8 @@
 // src/components/Login.js
 import React from 'react';
 import { supabase } from '../supabaseClient';
-import './Login.css'; // Use a relative path since Login.css is in the same folder
-import discordIcon from '../assets/discord-icon.png'; // Adjust the path relative to Login.js
+import './Login.css'; // Correct relative path
+import logo from '../assets/discord-icon.png'; // Correct relative path
 
 const Login = () => {
   const handleDiscordLogin = async () => {
@@ -19,7 +19,7 @@ const Login = () => {
 
   return (
     <button className="discord-login-button" onClick={handleDiscordLogin}>
-      <img src={discordIcon} alt="Discord Logo" className="discord-icon" />
+      <img src={logo} alt="Discord Logo" className="discord-icon" />
       Log in with Discord
     </button>
   );
