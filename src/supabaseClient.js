cf0d7b6a5fc72976f92cff66bbf81efac5259be3
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-20 19:43:39 +0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co'; // Replace with your project URL
-const supabaseAnonKey = 'your-anon-key'; // Replace with your anon public key
+const supabaseUrl = 'https://sudquzoonuxtvmjhvjpr.supabase.co'; // Replace with your project URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1ZHF1em9vbnV4dHZtamh2anByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNDg0ODMsImV4cCI6MjA1NTYyNDQ4M30.-9gQ6aQXagta6ZxxPNUw5qu40X0O04VfuoC3R63ZFss'; // Replace with your anon public key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
