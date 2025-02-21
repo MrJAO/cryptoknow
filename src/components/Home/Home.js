68f0cd9d8a83c0fd4f3f5fddc8f9a76f9bbdb4b2
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-21 15:02:50 +0800

    Create Home.js

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+// components/Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to CryptoKnow</h1>
+      <p>Your hub for airdrops, leaderboards, and more!</p>
+    </div>
+  );
+}
+
+export default Home;
