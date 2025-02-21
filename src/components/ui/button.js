c85a74822422f44865f60697e8d0f1480e861ffc
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-21 19:58:36 +0800

    Update button.js

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { cn } from "../lib/utils";
+import { cn } from "../../lib/utils";  // Adjust based on file location
 
 const Button = React.forwardRef(({ className, type = "button", ...props }, ref) => {
   return (
