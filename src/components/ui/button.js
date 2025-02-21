7c736ec652a682edb9f2165a3d7fca9da9a4bb31
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-21 18:35:50 +0800

    Update button.js

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Button = React.forwardRef(({ className, ...props }, ref) => {
+const Button = React.forwardRef(({ className, type = "button", ...props }, ref) => {
   return (
     <button
       ref={ref}
+      type={type}
       className={cn(
         "px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-all",
         className
