5d768c8a728022085d83546d6c031dbcd6bace8b
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-21 17:04:27 +0800

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 // src/components/Login.js
 import React from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../../supabaseClient';
 
 const Login = () => {
   const handleDiscordLogin = async () => {
