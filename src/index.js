deab16fa1e9eac009b4a9e73992b64c4338760ff
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-21 20:49:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
