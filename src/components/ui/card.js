214d86990d4833543918e5f82a361afe49e02c6e
Mr. JAO <39844929+MrJAO@users.noreply.github.com>
2025-02-21 18:30:51 +0800

    Update card.js

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -1,9 +1,15 @@
 import * as React from "react";
 
-export function Card({ children, className }) {
+const Card = ({ children, className = "" }) => {
   return (
-    <div className={`bg-white shadow-lg rounded-xl p-4 ${className}`}>
+    <div className={`bg-white shadow-md rounded-lg p-4 border ${className}`}>
       {children}
     </div>
   );
-}
+};
+
+const CardContent = ({ children, className = "" }) => {
+  return <div className={`p-4 ${className}`}>{children}</div>;
+};
+
+export { Card, CardContent };
