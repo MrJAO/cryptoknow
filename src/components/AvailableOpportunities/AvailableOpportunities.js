d4fca0c5be4d02d818ea6842e4c69309c3a33774
Mr. JAO <opriljoshua@gmail.com>
2025-02-24 15:11:15 +0800

    Fixing columns mismatches

--- src/components/AvailableOpportunities/AvailableOpportunities.js
+++ src/components/AvailableOpportunities/AvailableOpportunities.js
@@ -60,8 +60,8 @@ function AvailableOpportunities() {
             <tr style={{ background: "#333", color: "#fff", textAlign: "left" }}>
               <th style={{ padding: "12px", width: "30%" }}>Project Name</th>
               <th style={{ padding: "12px", width: "20%" }}>Type</th>
-              <th style={{ padding: "12px", width: "30%" }}>Link</th>
-			  <th style={{ padding: "12px", width: "20%" }}>End Date</th>
+              <th style={{ padding: "12px", width: "30%" }}>Link</th>			  
+              <th style={{ padding: "12px", width: "20%" }}>End Date</th>
             </tr>
           </thead>
           <tbody>
@@ -69,9 +69,6 @@ function AvailableOpportunities() {
               <tr key={opportunity.id} style={{ borderBottom: "1px solid #ddd" }}>
                 <td style={{ padding: "12px", fontWeight: "bold" }}>{opportunity.project_name}</td>
                 <td style={{ padding: "12px" }}>{opportunity.type}</td>
-                <td style={{ padding: "12px", color: "#d9534f", fontWeight: "bold" }}>
-                  {opportunity.end_date ? opportunity.end_date : "N/A"}
-                </td>
                 <td style={{ padding: "12px" }}>
                   <a
                     href={opportunity.link}
@@ -81,6 +78,9 @@ function AvailableOpportunities() {
                   >
                     Visit
                   </a>
+                </td>				
+                <td style={{ padding: "12px", color: "#d9534f", fontWeight: "bold" }}>
+                  {opportunity.end_date ? opportunity.end_date : "N/A"}
                 </td>
               </tr>
             ))}
