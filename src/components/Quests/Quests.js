38d4d34855354c5442599a95a0e02e9e85aa117d
Mr. JAO <opriljoshua@gmail.com>
2025-02-24 20:38:33 +0800

    Quests new approach

--- src/components/Quests/Quests.js
+++ src/components/Quests/Quests.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../../supabaseClient";
 
 const TwitterQuestForm = () => {
   const [user, setUser] = useState(null);
